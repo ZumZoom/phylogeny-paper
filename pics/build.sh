835ee253a15bc14faab2a6d0ae46e2bbe1c6c1a2
Mikhail Melnik <by.zumzoom@gmail.com>
2015-02-26 03:15:23 +0300

    major fixes from Chivilikhin and Buzhinsky

--- pics/build.sh
+++ pics/build.sh
@@ -6,3 +6,7 @@ dot -Tps:cairo inp3.gv > tmp.ps
 ps2epsi tmp.ps inp3.eps
 dot -Tps:cairo ans.gv > tmp.ps
 ps2epsi tmp.ps ans.eps
+dot -Tps:cairo inp_dummy.gv > tmp.ps
+ps2epsi tmp.ps inp_dummy.eps
+dot -Tps:cairo ans_dummy.gv > tmp.ps
+ps2epsi tmp.ps ans_dummy.eps
