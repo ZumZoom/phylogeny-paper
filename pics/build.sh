1e716d20db8e93c029a22c342e05556fc8fbd885
Mikhail Melnik <by.zumzoom@gmail.com>
2015-03-01 12:41:24 +0300

    final fixes

--- pics/build.sh
+++ pics/build.sh
@@ -1,12 +1,14 @@
-dot -Tps:cairo inp1.gv > tmp.ps
+dot -Tps inp1.gv > tmp.ps
 ps2epsi tmp.ps inp1.eps
-dot -Tps:cairo inp2.gv > tmp.ps
+dot -Tps inp2.gv > tmp.ps
 ps2epsi tmp.ps inp2.eps
-dot -Tps:cairo inp3.gv > tmp.ps
+dot -Tps inp3.gv > tmp.ps
 ps2epsi tmp.ps inp3.eps
-dot -Tps:cairo ans.gv > tmp.ps
+dot -Tps ans.gv > tmp.ps
 ps2epsi tmp.ps ans.eps
-dot -Tps:cairo inp_dummy.gv > tmp.ps
+dot -Tps inp_dummy.gv > tmp.ps
 ps2epsi tmp.ps inp_dummy.eps
-dot -Tps:cairo ans_dummy.gv > tmp.ps
+dot -Tps ans_dummy.gv > tmp.ps
 ps2epsi tmp.ps ans_dummy.eps
+pdf2ps up_var.pdf
+ps2epsi up_var.ps up_var.eps
